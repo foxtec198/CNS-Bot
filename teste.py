d97b1c68f18b9c7687f666529e775870c9007236
Guilherme Breve <foxtec198@gmail.com>
2024-04-05 13:38:56 -0300

    .

--- teste.py
+++
@@ -1,8 +0,0 @@
-def cns_qrcode(msg):
-    msg2 = msg.split(' ')
-    param = msg2[1].split(':')
-    CR = param[0]
-    if len(param) == 2: Nivel = param[1]
-    else: Nivel = 3
-
-cns_qrcode('/qrcode 42636:5')
